763440a612aa703215e0ae3fe9ea1f15b62407b6
ARLETH <arleth.troncosov@utem.cl>
2022-05-13 17:26:45 -0400

    5to commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,6 @@ $(document).scroll( function(e){
 var myCarousel = document.querySelector('#myCarousel')
 var carousel = new bootstrap.Carousel(myCarousel)
 
-
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
  
 //Tooltip redes sociales
 $(document).ready(function(){
